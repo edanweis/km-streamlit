eabaff81eac6c54f07b0eef3a5804d4cb5a7f5ac
Edan Weis <edanweis@gmail.com>
2021-08-23 13:05:29 +1000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,18 @@ from PIL import Image
 
 from txtai.embeddings import Embeddings
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
 
 import s3fs
 
 import boto3
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate('./aspect-km-4e35c3950fe3.json')
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
+# if not firebase_admin._apps:
+#     cred = credentials.Certificate('./aspect-km-4e35c3950fe3.json')
+#     firebase_admin.initialize_app(cred)
+#     db = firestore.client()
 
 
 def images(directory):
@@ -45,66 +45,82 @@ def images(directory):
 
 @st.cache(suppress_st_warning=True)
 def doSuccess():
-    st.balloons()
-    st.success('Ready')
+    placeholder = st.empty()
+    with placeholder.container():
+    # st.balloons()
+        placeholder.success('Ready')
+        placeholder.empty()
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+
+<<<<<<< HEAD
+def build(key):
+    status_text = st.empty()
+    progress_bar = st.progress(0) 
+    directory = f'{key}-embedding'
+=======
+@st.cache(hash_funcs={st.secrets.Secrets: id}, suppress_st_warning=True)
 def build(key):
     status_text = st.empty()
     progress_bar = st.progress(0) 
     directory = f'./{key}-embedding'
-    # if 'embeddings' in vars() or 'embeddings' in globals():
+>>>>>>> 6bbc65de2c338c4cd5edb7a5f310dbd70d074e16
+    # if 'embeddings' in vars() or 'e
+    # mbeddings' in globals():
     #     return embeddings
     # else:
     # 
     status_text.text('Mounting S3 file system')
-    fs = s3fs.S3FileSystem(anon=False, key= st.secrets["aws_access_key_id"], secret=st.secrets["aws_secret_access_key"])
+<<<<<<< HEAD
+    fs = s3fs.S3FileSystem(anon=False, key=st.secrets.aws_access_key_id, secret=st.secrets.aws_secret_access_key)
+=======
+    fs = s3fs.S3FileSystem(anon=False, key=st.secrets["aws_access_key_id"], secret=st.secrets["aws_secret_access_key"])
+>>>>>>> 6bbc65de2c338c4cd5edb7a5f310dbd70d074e16
     progress_bar.progress(20)
     status_text.text('Copying embeddings')
-    # if not os.path.isdir(directory):
-    #     os.makedirs(os.path.dirname(directory), exist_ok=True)
-    #     fs.get(f"s3://aspect-km/{directory}/config", f"./{directory}/config")
-    #     fs.get(f"s3://aspect-km/{directory}/embeddings", f"./{directory}/embeddings")
+    if not os.path.isdir(directory):
+        os.makedirs(os.path.dirname(f"./{directory}"), exist_ok=True)
+        fs.get(f"s3://aspect-km/{directory}/embeddings", f"./{directory}/embeddings")
+        fs.get(f"s3://aspect-km/{directory}/config", f"./{directory}/config")
+
     progress_bar.progress(60)
-    status_text.text('Downloading CLIP model from Sentence Transformers')
+    # status_text.text('Downloading CLIP model from Sentence Transformers')
     # clippath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'clip-ViT-B-32')
     embeddings = Embeddings()
     progress_bar.progress(80)
-    status_text.text('Indexing local image embeddings')
-    embeddings.load(directory)
+    embeddings.load(f"./{directory}")
     progress_bar.progress(100)
-    progress_bar = None
-    status_text = None
+    progress_bar.empty()
+    status_text.text('')
     doSuccess()
     return embeddings
 
-@st.cache(allow_output_mutation=True, hash_funcs={firebase_admin.App: id})
-def db():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate('./aspect-km-4e35c3950fe3.json')
-        firebase_admin.initialize_app(cred)
-    return firestore.client()
+# @st.cache(allow_output_mutation=True, hash_funcs={firebase_admin.App: id})
+# def db():
+#     if not firebase_admin._apps:
+#         cred = credentials.Certificate('./aspect-km-4e35c3950fe3.json')
+#         firebase_admin.initialize_app(cred)
+#     return firestore.client()
 
 
-@st.cache(hash_funcs={firebase_admin.App: id, s3fs.core.S3File: id})
-def firebaseCallback(results, app_state):
-    app_state = get_app_state()
-    if app_state['s']:
-        doc_ref = db().collection(u'streamlit').document(app_state['s'])
-        doc_ref.set({
-            u'results': results
-        }, merge=True)
+# @st.cache(hash_funcs={firebase_admin.App: id, s3fs.core.S3File: id})
+# def firebaseCallback(results, app_state):
+#     app_state = get_app_state()
+#     if app_state['s']:
+#         doc_ref = db().collection(u'streamlit').document(app_state['s'])
+#         doc_ref.set({
+#             u'results': results
+#         }, merge=True)
 
 def get_app_state():
     app_state = st.experimental_get_query_params()
     app_state = {k: v[0] if isinstance(v, list) else v for k, v in app_state.items()}
     return app_state
 
-@st.cache(allow_output_mutation=True)
+@st.cache(hash_funcs={"_thread.RLock": lambda _: None}, allow_output_mutation=True)
 def generate_presigned_url(object_key, bucket_name='aspect-km', expiry=3600):
     client = boto3.client("s3",region_name='ap-southeast-2',
-                          aws_secret_access_key=st.secrets["aws_secret_access_key"],
-                          aws_access_key_id=st.secrets["aws_access_key_id"])
+                          aws_secret_access_key=st.secrets.aws_secret_access_key,
+                          aws_access_key_id=st.secrets.aws_access_key_id)
     try:
         response = client.generate_presigned_url('get_object',
                           Params={'Bucket': bucket_name,'Key': object_key},
@@ -137,14 +153,10 @@ def app():
     query = st.text_input("")
 
     if query:
-        # images, labels = [] 
-        for result in embeddings.search(query, 5):
+        for result in embeddings.search(query, 10):
             index, _ = result
             st.write(index)
             st.image(generate_presigned_url(f"precedent-images/{Path(index).name}"))
-            # st.image(fs.open(f"s3://aspect-km/precedent-images/{Path(index).name}", "rb"))
-        firebaseCallback({"index": index.info()['name'], "query":query, "_": _, "s": app_state['s'], "embeddings": embeddings_path }, app_state)
-
 
 if __name__ == "__main__":
     app()
