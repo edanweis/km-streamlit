266b2f95393defa13c1f52960c894bbf7c13eb92
Edan Weis <edanweis@gmail.com>
2021-08-25 23:52:45 +1000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ def app():
     hide_menu_style = """
             <style>
                 .css-1e5imcs, .e1tzin5v1 {margin: 0 !important;}
-                a[class^="viewerBadge_container__1QSob"] a {display: none !important; opacity: 0 !important}
+                a[class^="viewerBadge_container"] {display: none !important; opacity: 0 !important}
                 [data-baseweb="notification"] {margin-top: 60px !important;}
                 .stTextInput {position: absolute !important; z-index: 1 !important;}
                 #root > div:nth-child(1) {color: transparent !important}
