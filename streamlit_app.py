60809535529386b7ce510c4f41aa2d817769b68d
Edan Weis <edanweis@gmail.com>
2021-08-25 12:06:09 +1000

    change creds

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ from google.oauth2 import service_account
 if not firebase_admin._apps:
     # Create credentials object then initialize the firebase admin client
     sec_client = secretmanager.SecretManagerServiceClient()
-    name = sec_client.secret_version_path('707318120192', 'firebase-adminsdk', "latest")
-    response = sec_client.access_secret_version(name)
+    response = sec_client.access_secret_version(request={"name": "projects/707318120192/secrets/aspect-km-firebase-admin-service/versions/1"})
     service_account_info = json.loads(response.payload.data.decode('utf-8'))
     # build credentials with the service account dict
     creds = firebase_admin.credentials.Certificate(service_account_info)
@@ -50,8 +49,7 @@ def db():
     if not firebase_admin._apps:
         # Create credentials object then initialize the firebase admin client
         sec_client = secretmanager.SecretManagerServiceClient()
-        name = sec_client.secret_version_path('707318120192', 'firebase-adminsdk', "latest")
-        response = sec_client.access_secret_version(name)
+        response = sec_client.access_secret_version(request={"name": "projects/707318120192/secrets/aspect-km-firebase-admin-service/versions/1"})
         service_account_info = json.loads(response.payload.data.decode('utf-8'))
         # build credentials with the service account dict
         creds = firebase_admin.credentials.Certificate(service_account_info)
