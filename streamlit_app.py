8327ff3f648c3f210f2dbba6049464c8547d6c88
Edan Weis <edanweis@gmail.com>
2021-08-26 13:04:25 +1000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,8 @@ def app():
         #     st.write(index)
         #     image = generate_presigned_url(f"precedent-images/{Path(index).name}")
         #     cols[i].image(image)
-        firebaseCallback({'results': [{"filepath": k, "score": v, "url": generate_presigned_url(f"precedent-images/{Path(k).name}") } for k,v in results], 'query': query})
+
+        firebaseCallback({'results': [{"format": "".join([s for s in Path(k).suffixes if not " " in s]), "filepath": k, "score": v, "url": generate_presigned_url(f"precedent-images/{Path(k).name}") } for k,v in results], 'query': query})
         # st.write({"query":query, "_": _, "embeddings": embeddings_path, **app_state})
 
 if __name__ == "__main__":
